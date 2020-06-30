b89d97195609d89e51e8f70ec72b06da5dc90e19
Joel Deans <JHDeans@users.noreply.github.com>
2020-06-29 21:21:55 -0600

    Updates Added

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 
-export default class App extends Component {
-  render() {
-    return (
+export default App = () => {
+  render( 
+   
       <div className='app'>
         <h1>DevCamp React Starter</h1>
         <h2>React Redux Router</h2>
       </div>
     );
-  }
+  
 }
